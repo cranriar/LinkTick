00def9ef22fe11d6cbcc1b9627b2a277fd5bd784
cranriar <69156990+cranriar@users.noreply.github.com>
2024-11-11 18:53:42 -0500

    Actualizar ValueObjects

---
+++ src/BoundedContext/Order/Domain/ValueObjects/OrderTotal.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\BoundedContext\Order\Domain\ValueObjects;
+
+use InvalidArgumentException;
+
+final class OrderTotal
+{
+    private $value;
+
+    /**
+     * OrderTotal constructor.
+     * @param float $total
+     * @throws InvalidArgumentException
+     */
+    public function __construct(float $total)
+    {
+        $this->validate($total);
+        $this->value = $total;
+    }
+
+    /**
+     * @param float $total
+     * @throws InvalidArgumentException
+     */
+    private function validate(float $total): void
+    {
+        $options = array(
+            'options' => array(
+                'min_range' => 1.00,
+'max_range' => 1000000.00
+)
+        );
+
+        if (!filter_var($id, FILTER_VALIDATE_FLOAT, $options)) {
+            throw new InvalidArgumentException(
+                sprintf('<%s> does not allow the value <%s>.', static::class, $total)
+            );
+        }
+    }
+
+    public function value(): float
+    {
+        return $this->value;
+    }
+
+}
